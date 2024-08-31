78be473a34a5480c86f6e04c23f547041df50a0a
vansh1505 <86408597+vansh1505@users.noreply.github.com>
2024-08-31 14:32:35 +0530

    React app setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+function App() {
+
+  return (
+    <>
+    <Navbar />
+      <h1 className="bg-red-300">hello</h1>
+    </>
+  )
+}
+
+export default App

c45ebeff8dbacb14fb2de68c162e73f9baf9feba
vansh1505 <86408597+vansh1505@users.noreply.github.com>
2024-09-24 21:52:48 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -5,21 +5,26 @@ const client = new MongoClient(uri);
 
 async function fetchData() {
   try {
+    console.log('Connecting to MongoDB...');
     await client.connect();
-    console.log('Connected to MongoDB'); // Log successful connection
+    console.log('Connected to MongoDB');
+
     const database = client.db('Museum-data');
     const collection = database.collection('database');
+
+    console.log('Fetching data...');
     const data = await collection.find({}).toArray();
-    console.log('Data fetched successfully:', data); // Log fetched data
+    console.log('Data fetched successfully:', data);
     return data;
   } catch (err) {
-    console.error('Error occurred while fetching data:', err); // Log the error
+    console.error('Error occurred while fetching data:', err);
     throw new Error('Failed to fetch data from the database.');
   } finally {
     await client.close();
-    console.log('MongoDB connection closed'); // Log connection closure
+    console.log('MongoDB connection closed');
   }
 }
+
 
 module.exports = async (req, res) => {
   if (req.method === 'GET') {
